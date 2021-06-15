9c8a23def6438da5ac5662838740670e219c3e9e
aki-space-crea <aki.chan.w.13@gmail.com>
2021-06-15 23:40:17 +0900

    GAタグを追加。

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ChakraProvider, Box, ColorModeScript } from '@chakra-ui/react'
 import theme from '../src/theme/theme'
 
+import { GA_TRACKING_ID } from '../lib/gtag'
+
 import Layout from '../src/components/Layout'
 
 class MyDocument extends Document {
@@ -13,7 +15,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
+        </Head>
         <ChakraProvider theme={theme}>
           <Box as="body">
             <ColorModeScript initialColorMode={theme.config.initialColorMode} />
