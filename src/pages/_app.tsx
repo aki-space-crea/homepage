7fd9dc0feaa2c58255acd371be88a7c1abdc6d56
kuroda_t <kuroda_t@mti.co.jp>
2022-09-13 21:15:34 +0900

    GA導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,23 @@ import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
+import { GA_TRACKING_ID, pageview } from '../logics/gtag'
 
 const App = (props: AppProps): JSX.Element => {
   const { Component, pageProps } = props
+  const router = useRouter()
+  useEffect(() => {
+    // GA_TRACKING_ID が設定されていない場合は、処理終了
+    if (!GA_TRACKING_ID) return
+
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <>
