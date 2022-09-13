6433f8c86d23a9cb4272f8c567e39390df38a944
kuroda_t <kuroda_t@mti.co.jp>
2022-09-13 21:57:34 +0900

    googleTagManegerの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { useRouter } from 'next/router'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 import { GA_TRACKING_ID, pageview } from '../logics/gtag'
+import GoogleTagManager, { GoogleTagManagerId } from 'components/googleTagManager'
+import { googleTagManagerId } from 'utils'
 
 const App = (props: AppProps): JSX.Element => {
   const { Component, pageProps } = props
@@ -24,6 +26,7 @@ const App = (props: AppProps): JSX.Element => {
   return (
     <>
       <Head>
+        <GoogleTagManager googleTagManagerId={googleTagManagerId as GoogleTagManagerId} />
         <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
